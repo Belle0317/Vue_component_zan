b09c2a63cd224ae5c6100537a97cfebacd4a0c90
Belle0317 <46584373+Belle0317@users.noreply.github.com>
2019-01-22 11:53:31 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// Vue.component('alert', );
+
+Vue.component('like', {
+	template: '#like_component_zan',
+	data: function() {
+		return {
+			like_count: 10,
+			liked: false,
+		}
+	},
+	methods: {
+		toggle_like: function() {
+			if (!this.liked)
+				this.like_count++;
+			else
+				this.like_count--;
+
+			this.liked = !this.liked;
+
+
+		}
+
+	}
+})
+
+new Vue({
+	el: '#app',
+
+});
